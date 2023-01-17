e13dcdb02588fdd5822a1caf97552a8d322d92eb
tony.origogi <tony.origogi@kakaobrain.com>
2023-01-17 18:53:07 +0900

    암묵적 입력, 출력을 제거

--- functional_programing/example/megamart/code.js
+++ functional_programing/example/megamart/code.js
@@ -30,13 +30,17 @@ function update_tax_dom() {
 }
 
 function calc_cart_total() {
-  shopping_cart_total = 0;
-
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
-  }
+  shopping_cart_total = calc_total(shopping_cart);
   set_cart_total_dom();
   update_shipping_icons();
   update_tax_dom();
+}
+
+function calc_total(cart) {
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var item = cart[i];
+    shopping_cart_total += item.price;
+  }
+  return total;
 }
