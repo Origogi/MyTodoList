793a4511f8421483be5f89041ff5d49e47a9ff85
tony.origogi <tony.origogi@kakaobrain.com>
2023-01-28 11:21:17 +0900

    update

--- functional_programing/example/chap8/code.js
+++ functional_programing/example/chap8/code.js
@@ -2,6 +2,21 @@ function isInCart(cart, name) {
   return indexOfItem(cart, name) !== null;
 }
 
+function setPriceByName(cart, name, price) {
+  var i = indexOfItem(cart, name);
+
+  if (i !== null) {
+    return arraySet(cartCopy, i, setPrice(cartCopy[i], price));
+  }
+  return cartCopy;
+}
+
+function arraySet(array, idx, value) {
+  var copy = array.slice();
+  copy[idx] = value;
+  return copy;
+}
+
 function indexOfItem(cart, name) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].name === name) {
