dd61cd411f74c011e94e8c993ec306e3ea9ad9b9
tony.origogi <tony.origogi@kakaobrain.com>
2023-02-02 08:01:12 +0900

    update

--- functional_programing/example/chap10/code.js
+++ functional_programing/example/chap10/code.js
@@ -55,3 +55,51 @@ forEach(dishes, function (dish) {
   dryDish(dish);
   putAway(dish);
 });
+
+function withArrayCopy(array, modify) {
+  var copy = array.slice();
+  modify(copy);
+  return copy;
+}
+
+function arraySet(array, idx, value) {
+  return withArrayCopy(array, function (copy) {
+    copy[idx] = value;
+  });
+}
+
+function push(array, elem) {
+  return withArrayCopy(array, function (copy) {
+    copy.push(elem);
+  });
+}
+
+function drop_last(array) {
+  return withArrayCopy(array, function (copy) {
+    copy.pop();
+  });
+}
+
+function drop_first(array) {
+  return withArrayCopy(array, function (copy) {
+    copy.shift();
+  });
+}
+
+function objectSet(object, key, value) {
+  return withObjectCopy(object, function (copy) {
+    copy[key] = value;
+  });
+}
+
+function objectDelete(object, key) {
+  return withObjectCopy(object, function (copy) {
+    delete copy[key];
+  });
+}
+
+function withObjectCopy(object, modify) {
+  var copy = Object.assign({}, object);
+  modify(copy);
+  return copy;
+}
