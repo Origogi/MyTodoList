dc342acc7499d23337884409a4ce5933c18d271b
tony.origogi <tony.origogi@kakaobrain.com>
2023-02-10 17:58:26 +0900

    update

--- functional_programing/example/chap13/code.js
+++ functional_programing/example/chap13/code.js
@@ -2,4 +2,22 @@ function biggestPurchaseBestCustomers(customers) {
   var bestCustomers = filter(customers, function (customer) {
     return customer.purchases.length > 3;
   });
+
+  var biggerPurchases = map(bestCustomers, function (customer) {
+    return maxKey(customer.purchases, { total: 0 }, function (purchase) {
+      return purchase.total;
+    });
+  });
+
+  return biggerPurchases;
+}
+
+function maxKey(array, init, f) {
+  return reduce(array, init, function (biggestSoFar, elelment) {
+    if (f(biggestSoFar) > f(elelment)) {
+      return biggestSoFar;
+    } else {
+      elelment;
+    }
+  });
 }
