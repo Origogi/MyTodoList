9a0a14edb7af7512247690f49caa0daac3a569ea
tony.origogi <tony.origogi@kakaobrain.com>
2023-02-12 11:29:17 +0900

    update

--- functional_programing/example/chap13/code-2.js
+++ functional_programing/example/chap13/code-2.js
@@ -23,13 +23,12 @@ function sampleCode2(array, window) {
     var sum = 0;
     var count = 0;
 
-    for (var w = 0; w < window; w++) {
-      var idx = i + w;
-      if (idx < array.length) {
-        sum += array[idx];
+    var subarray = array.slice(i, i + window);
+    for (var w = 0; w < subarray.length; w++) {
+        sum += subarray[w];
         count += 1;
-      }
     }
+
     answer.push(sum / count);
   }
 
