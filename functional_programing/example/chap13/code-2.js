18e330558b2a2511058f52057a20d87047c63808
tony.origogi <tony.origogi@kakaobrain.com>
2023-02-16 12:03:24 +0900

    update

--- functional_programing/example/chap13/code-2.js
+++ functional_programing/example/chap13/code-2.js
@@ -32,7 +32,7 @@ function sampleCode2(array, window) {
 
 function range(start, end) {
   var ret = [];
-  for (var i = start; i<end;i++) {
+  for (var i = start; i < end; i++) {
     ret.push(i);
   }
   return ret;
@@ -76,3 +76,42 @@ const window = 5;
 
 console.log(sampleCode(input, window));
 console.log(sampleCode2(input, window));
+
+function shoesAndSocksInventoryBefore(products) {
+  var inventory = 0;
+  for (var p = 0; p < products.length; p++) {
+    var product = products[p];
+
+    if (product.type === 'shoes' || product.type === 'socks') {
+      inventory += product.numberInventory;
+    }
+  }
+  return inventory;
+}
+
+function shoesAndSocksInventory(products) {
+  var shoesAndSocks = filter(products, function (product) {
+    return product.type === 'shoes' || product.type === 'socks';
+  });
+
+  var inventories = map(shoesAndSocks, function (product) {
+    return product.numberInventory;
+  });
+
+  return reduce(inventories, 0, plus);
+}
+
+var shoppingCart = reduce(itemAdded, {}, addOne);
+
+function addOne(cart, item) {
+  if (!cart[item]) {
+    return add_item(cart, {
+      name: item,
+      qunatity: 1,
+      price: priceLookup(item),
+    });
+  } else {
+    var qunatity = cart[item].qunatity;
+    return setFieldByName(cart, item, 'qunatity', qunatity + 1);
+  }
+}
