86f20c4897d912e67b180a495f1db44ae5a4c132
Origogi <rlawjdxo1212@naver.com>
2023-02-16 10:21:37 +0900

    Update

--- functional_programing/example/chap13/code-2.js
+++ functional_programing/example/chap13/code-2.js
@@ -19,20 +19,56 @@ function sampleCode(array, window) {
 
 function sampleCode2(array, window) {
   var answer = [];
-  for (var i = 0; i < array.length; i++) {
-    var sum = 0;
-    var count = 0;
 
+  var indices = range(0, array.length);
+
+  var answer = map(indices, function (i) {
     var subarray = array.slice(i, i + window);
-    for (var w = 0; w < subarray.length; w++) {
-        sum += subarray[w];
-        count += 1;
-    }
-
-    answer.push(sum / count);
-  }
+    return average(subarray);
+  });
 
   return answer;
+}
+
+function range(start, end) {
+  var ret = [];
+  for (var i = start; i<end;i++) {
+    ret.push(i);
+  }
+  return ret;
+}
+
+function average(array) {
+  return sum(array) / array.length;
+}
+
+function sum(array) {
+  return reduce(array, 0, function (e, acc) {
+    return acc + e;
+  });
+}
+
+function forEach(array, f) {
+  for (var i = 0; i < array.length; i++) {
+    var item = array[i];
+    f(item);
+  }
+}
+
+function map(array, f) {
+  var newArray = [];
+  forEach(array, function (element) {
+    newArray.push(f(element));
+  });
+  return newArray;
+}
+
+function reduce(array, init, f) {
+  var result = init;
+  for (var i = 0; i < array.length; i++) {
+    result = f(array[i], result);
+  }
+  return result;
 }
 
 const input = [1, 2, 3, 4, 5];
