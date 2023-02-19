c777446a59c98bdd274e6052679600582b0dd583
tony.origogi <tony.origogi@kakaobrain.com>
2023-02-19 10:51:35 +0900

    update

--- functional_programing/example/chap16/code.js
+++ functional_programing/example/chap16/code.js
@@ -1,6 +1,6 @@
 function add_item_to_cart(item) {
   cart = add_item(cart, item);
-  calc_cart_total();
+  update_total_queue(cart);
 }
 
 function calc_cart_total(cart, callback) {
@@ -15,28 +15,73 @@ function calc_cart_total(cart, callback) {
   });
 }
 
-var queue_items = [];
-var working = false;
+function Queue(worker) {
+  var queue_items = [];
+  var working = false;
 
-function runNext() {
-  if (working) {
-    return;
+  function runNext() {
+    if (working) {
+      return;
+    }
+
+    if (queue_items.length === 0) {
+      return;
+    }
+
+    working = true;
+    var item = queue_items.shift();
+
+    worker(item.data, function (val) {
+      working = false;
+      setTimeout(item.callback, 0, val);
+      runNext();
+    });
   }
 
-  if (queue_items.length === 0) {
-    return;
+  return function (data, callback) {
+    queue_items.push({ data: data, callback: callback || function () {} });
+    setTimeout(runNext, 0);
+  };
+}
+
+function DroppingQueue(max, worker) {
+  var queue_items = [];
+  var working = false;
+
+  function runNext() {
+    if (working) {
+      return;
+    }
+
+    if (queue_items.length === 0) {
+      return;
+    }
+
+    working = true;
+    var item = queue_items.shift();
+
+    worker(item.data, function (val) {
+      working = false;
+      setTimeout(item.callback, 0, val);
+      runNext();
+    });
   }
 
-  working = true;
-  var cart = queue_items.shift();
+  return function (data, callback) {
+    queue_items.push({ data: data, callback: callback || function () {} });
+    while (queue_items.length > max) {
+      queue_items.shift();
+    }
+
+    setTimeout(runNext, 0);
+  };
+}
+
+function calc_cart_worker(cart, done) {
   calc_cart_total(cart, function (total) {
     update_total_dom(total);
-    working = false;
-    runNext();
+    done();
   });
 }
 
-function update_total_queue(cart) {
-  queue_items.push(cart);
-  setTimeout(runNext, 0);
-}
+var update_total_queue = Queue(calc_cart_worker);
