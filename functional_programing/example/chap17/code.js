c740a49502e7f059cb0a8f4a08037de6dadf95a2
tony.origogi <tony.origogi@kakaobrain.com>
2023-02-19 17:08:29 +0900

    update

---
+++ functional_programing/example/chap17/code.js
@@ -0,0 +1,104 @@
+function add_item_to_cart(item) {
+  cart = add_item(cart, item);
+  update_total_queue(cart);
+}
+
+function calc_cart_total(cart, callback) {
+  var total = 0;
+
+  var done = Cut(2, function () {
+    callback(total);
+  });
+
+  cost_ajax(cart, function (cost) {
+    total += cost;
+    done();
+  });
+
+  shipping_ajax(cart, function (shipping) {
+    total += shipping;
+    done();
+  });
+}
+
+function Queue(worker) {
+  var queue_items = [];
+  var working = false;
+
+  function runNext() {
+    if (working) {
+      return;
+    }
+
+    if (queue_items.length === 0) {
+      return;
+    }
+
+    working = true;
+    var item = queue_items.shift();
+
+    worker(item.data, function (val) {
+      working = false;
+      setTimeout(item.callback, 0, val);
+      runNext();
+    });
+  }
+
+  return function (data, callback) {
+    queue_items.push({ data: data, callback: callback || function () {} });
+    setTimeout(runNext, 0);
+  };
+}
+
+function DroppingQueue(max, worker) {
+  var queue_items = [];
+  var working = false;
+
+  function runNext() {
+    if (working) {
+      return;
+    }
+
+    if (queue_items.length === 0) {
+      return;
+    }
+
+    working = true;
+    var item = queue_items.shift();
+
+    worker(item.data, function (val) {
+      working = false;
+      setTimeout(item.callback, 0, val);
+      runNext();
+    });
+  }
+
+  return function (data, callback) {
+    queue_items.push({ data: data, callback: callback || function () {} });
+    while (queue_items.length > max) {
+      queue_items.shift();
+    }
+
+    setTimeout(runNext, 0);
+  };
+}
+
+function calc_cart_worker(cart, done) {
+  calc_cart_total(cart, function (total) {
+    update_total_dom(total);
+    done();
+  });
+}
+
+var update_total_queue = Queue(calc_cart_worker);
+
+function Cut(num, callback) {
+  var num_finished = 0;
+
+  return function () {
+    num_finished += 1;
+    if (num_finished === num) {
+      callback();
+    }
+  };
+}
---
+++ functional_programing/example/chap18/code.js
@@ -0,0 +1,41 @@
+function ValueCell(initialValue) {
+  var currentValue = initialValue;
+
+  var watchers = [];
+
+  return {
+    val: function () {
+      return currentValue;
+    },
+    update: function (f) {
+      var oldValue = currentValue;
+      var newValue = f(oldValue);
+
+      if (oldValue !== newValue) {
+        currentValue = newValue;
+
+        forEach(watchers, function (watcher) {
+          watcher(newValue);
+        });
+      }
+    },
+    addWatcher: function (f) {
+      watchers.push(f);
+    },
+  };
+}
+
+var shopping_cart = ValueCell([]);
+
+function add_item_to_cart(name, price) {
+  var item = make_cart_item(name, price);
+
+  shopping_cart.update(function (cart) {
+    return add_item(cart, item);
+  });
+  var total = calc_cart_total(shopping_cart.val());
+  set_cart_total_dom(total);
+  update_tax_dom(total);
+}
+
+shopping_cart.addWatcher(update_shipping_icons);
