318905f244e315b4690f07484a06198cfc773d3f
tony.origogi <tony.origogi@kakaobrain.com>
2023-04-12 20:59:06 +0900

    update

---
+++ typescript/1-TYPES/1-1-basic.ts
@@ -0,0 +1,21 @@
+{
+  let name = 'hello';
+  name = 'hi';
+
+  // number
+  const num: number = 6;
+  // String
+  const str: string = 'hello';
+  // Boolean
+  const boal: boolean = false;
+
+  // undefined 아직 값이 결정되지 않음
+
+  let age : number | undefined;
+  age = 3;
+  age = undefined;
+
+  // null : 값이 명확하게 없을 때 사용
+  let person2: string | null;
+
+}
--- typescript/README.md
+++ typescript/README.md
@@ -1,4 +1,6 @@
-# TypeScript Complier
+# TypeScript
+
+## Compiler
 
 - tsc 는 타입스크립트 컴파일러이다.
 - ts 파일은 브라우져(node) 에서 바로 실행이 불가능하다.
@@ -9,3 +11,7 @@ tsc main.ts
 ```
 
 위 명령어를 실행하면 main.js 파일이 생성된다.
+
+### ts-node
+
+ts 파일을 js 로 변환하고 바로 실행하는 tool 이다.
