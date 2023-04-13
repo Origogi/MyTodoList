bcb15df4e230437187924703b12324b2488b9893
tony.origogi <tony.origogi@kakaobrain.com>
2023-04-13 10:55:32 +0900

    update

--- typescript/1-TYPES/1-2-functions.ts
+++ typescript/1-TYPES/1-2-functions.ts
@@ -5,22 +5,47 @@
   }
 
   // Javascript
-  function jsFetchNum(id) {
+//   function jsFetchNum(id) {
+//     // code ...
+//     // code ...
+//     // code ...
+//     return new Promise((resolve, reject) => {
+//       resolve(100);
+//     });
+//   }
+
+  // TypeScript
+  function tsFetchNum(id: string): Promise<number> {
     // code ...
     // code ...
     // code ...
     return new Promise((resolve, reject) => {
       resolve(100);
     });
-
-    // TypeScript
-    function tsFetchNum(id: string): Promise<number> {
-      // code ...
-      // code ...
-      // code ...
-      return new Promise((resolve, reject) => {
-        resolve(100);
-      });
-    }
   }
+
+  // Optional Parameter
+  function printName(firstName: string, lastName?: string) {
+    console.log(firstName);
+    console.log(lastName);
+  }
+
+  printName('Steve', 'Jobs');
+  printName('Ellie');
+
+  // Default Parameters
+  function printMessage(message: string = 'default message') {
+    console.log(message);
+  }
+
+  printMessage();
+
+  // Rest Parameters
+  function addNumbers(...numbers: number[]): number {
+    return numbers.reduce((a, b) => a + b);
+  }
+  console.log(addNumbers(1, 2));
+
+//   console.log(addNumbers(1, 2. '3')); Compile Error
+  console.log(addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 }
