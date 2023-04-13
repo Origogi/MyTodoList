47f7f02cf70e980a8e9b08bddec90717fa8e6961
tony.origogi <tony.origogi@kakaobrain.com>
2023-04-13 11:41:19 +0900

    update

---
+++ typescript/1-TYPES/1-3-array.ts
@@ -0,0 +1,14 @@
+{
+    const fruits: string[] = ['🍎', '🍌'];
+    const scores: number[] = [1, 3, 4];
+
+    function printArray(fruits: readonly string[]) {
+        // fruits.push(); // Error
+    }
+
+    // Tuple
+    let student : [string, number];
+    student = ['name', 123];
+    student[0] // name
+    
+}
