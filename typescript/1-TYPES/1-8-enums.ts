4c8a143d69646629ea4265210c046357799a0aac
tony.origogi <tony.origogi@kakaobrain.com>
2023-04-13 15:29:50 +0900

    update

---
+++ typescript/1-TYPES/1-8-enums.ts
@@ -0,0 +1,15 @@
+{
+  // 타입스트립트에서는 enum 대신 유니온을 더 추천한다. 왜냐하면 타입이 보장되지 않기 때문이다.
+  enum Days {
+    Monday = 3,
+    Tuesday, //1
+    Wednesdady,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+  let day: Days = 0; // 에러가 발생하지 않는다.
+  day = Days.Friday;
+  day = 14;  // 에러가 발생하지 않는다.
+  console.log(Days.Friday);
+}
