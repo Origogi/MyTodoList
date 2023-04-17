25d250c4787c458ae8630fcd9eb18834c7fb28dd
tony.origogi <tony.origogi@kakaobrain.com>
2023-04-17 14:57:54 +0900

    update

---
+++ typescript/3-OOP/3-2-class.ts
@@ -0,0 +1,37 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAM_PER_SHOT = 7;
+    coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number ) {
+       return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(14);
+  const coffee = maker.makeCoffee(2);
+  const coffeeMaker = CoffeeMaker.makeMachine(14);
+  console.log(coffeeMaker.makeCoffee(2));
+  
+}
