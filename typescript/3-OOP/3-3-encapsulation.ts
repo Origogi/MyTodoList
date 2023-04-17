8315b0f065ff31510e6f5cdf0c7df048359bcb16
tony.origogi <tony.origogi@kakaobrain.com>
2023-04-17 15:08:56 +0900

    update

--- typescript/3-OOP/3-3-encapsulation.ts
+++ typescript/3-OOP/3-3-encapsulation.ts
@@ -1,45 +1,64 @@
 {
-    type CoffeeCup = {
-        shots: number;
-        hasMilk: boolean;
-    };
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
 
-    // public
-    // private
-    // protected
-    class CoffeeMaker {
-        private  static BEANS_GRAM_PER_SHOT = 7; // class level
-        private coffeeBeans: number = 0;  // instance level
+  // public
+  // private
+  // protected
+  class CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT = 7; // class level
+    private coffeeBeans: number = 0; // instance level
 
-        private constructor(coffeeBeans: number) {
-            this.coffeeBeans = coffeeBeans;
-        }
-
-        static makeMachine(coffeeBeans: number ) {
-            return new CoffeeMaker(coffeeBeans);
-        }
-
-        fillCoffeeBeans(beans: number) {
-            if (beans < 0) {
-                throw new Error('value for beans should be greater than 0');
-            }
-            this.coffeeBeans += beans;
-        }
-
-        makeCoffee(shots: number): CoffeeCup {
-            if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
-                throw new Error('Not enough coffee beans!');
-            }
-
-            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
-            return {
-                shots,
-                hasMilk: false,
-            };
-        }
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
     }
 
-    const maker = CoffeeMaker.makeMachine(12);
-    maker.fillCoffeeBeans(34);
-    console.log(maker);
+    static makeMachine(coffeeBeans: number) {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  class User {
+    firstName: string;
+    lastName: string;
+
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  }
+
+  const user = new User("steven", "jobs");
+  console.log(user);
+  user.firstName = "Ellie";
+  console.log(user);
+
+  const maker = CoffeeMaker.makeMachine(12);
+  maker.fillCoffeeBeans(34);
+  console.log(maker);
 }
