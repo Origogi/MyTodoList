e1e29d8c0034ce0101dd730f026f0ba92ef564f7
tony.origogi <tony.origogi@kakaobrain.com>
2023-05-02 09:04:44 +0900

    update

--- typescript/12-motion-project/src/components/component.ts
+++ typescript/12-motion-project/src/components/component.ts
@@ -1,5 +1,7 @@
 export interface Component {
-    attachTo(parent : HTMLElement, position? : InsertPosition) : void;
+    attachTo(parent: HTMLElement, position?: InsertPosition): void;
+
+    removeFrom(parent: HTMLElement): void;
 }
 
 /**
@@ -7,13 +9,20 @@ export interface Component {
  */
 
 export class BaseComponent<T extends HTMLElement> implements Component {
-    protected readonly element : T
+    protected readonly element: T
 
-    constructor(htmlString : string) {
+    constructor(htmlString: string) {
 
         const template = document.createElement('template')
         template.innerHTML = htmlString;
         this.element = template.content.firstElementChild! as T;
+    }
+
+    removeFrom(parent: HTMLElement): void {
+        if (parent !== this.element.parentElement) {
+            throw new Error('Parent mismatch!');
+        }
+        parent.removeChild(this.element);
     }
 
     attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
