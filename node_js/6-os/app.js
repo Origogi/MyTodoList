9c1b7523271868ec079ca4b53de1b40ee139d10f
tony.origogi <tony.origogi@kakaobrain.com>
2023-05-08 10:49:57 +0900

    update

---
+++ node_js/6-os/app.js
@@ -0,0 +1,5 @@
+const os = require('os');
+console.log(os.EOL === '\n');
+
+console.log(os.cpus().length)
+console.log(os.totalmem())
--- node_js/7-process/app.js
+++ node_js/7-process/app.js
@@ -1,1 +1,15 @@
 const process = require('process');
+
+console.log(process.env);
+
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+
+process.nextTick(() => {
+    console.log('nextTick');
+});
+
+for (let i = 0; i < 100; i++) {
+    console.log('for loop')
+}
