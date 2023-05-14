9fcdfc20bbed146e90ab3e1a3156e6ae47485bf4
tony.origogi <tony.origogi@kakaobrain.com>
2023-05-14 10:31:00 +0900

    update

---
+++ node_js/challenge/photo.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+
+
+run('./test');
+
+function hasEditFile(files, file) {
+    const fileName = file.split('.')[0];
+    const fileId = fileName.split('_')[1];
+    const editFile = `IMG_E${fileId}.jpg`;
+    return files.includes(editFile);
+}
+
+async function run(folder) {
+    const files = await readFromFolder(folder);
+
+    for (const file of files) {
+        const ext = file.split('.')[1];
+
+        if (!ext) {
+            continue;
+        }
+
+        switch (ext) {
+            case 'mp4':
+            case 'mov':
+                await moveFile(folder, path.join(folder, 'video'), file);
+                break;
+            case 'png':
+            case 'aae':
+                await moveFile(folder, path.join(folder, 'captured'), file);
+                break;
+            case 'jpg':
+                if (hasEditFile(files, file)) {
+                    await moveFile(folder, path.join(folder, 'duplicated'), file);
+                }
+        }
+
+    }
+}
+
+async function createFolder(folder) {
+    if (fs.existsSync(folder)) {
+        return;
+    }
+    // 폴더 생성
+    fs.mkdirSync(folder);
+}
+
+async function moveFile(src, dest, file) {
+    await createFolder(dest);
+    console.log('move file : ', path.join(src,file), '=>', path.join(dest, file));
+
+    await fs.promises.rename(path.join(src,file), path.join(dest, file));
+}
+
+
+function readFromFolder(folder) {
+    return fs.promises.readdir(folder);
+}
