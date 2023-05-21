ceddbdd556f7a2ee8eeaa7eb2a0d0c2501bc5aac
tony.origogi <tony.origogi@kakaobrain.com>
2023-05-21 10:44:45 +0900

    update

---
+++ node_js/6-express/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my homepage!');
+});
+
+app.get('/sky/:id',(req, res, next) => {
+    console.log(req.params);
+    console.log(req.params.id);
+
+    console.log(req.query);
+    console.log(req.query.keyword);
+
+    res.send('hi');
+});
+
+app.listen(8080);
