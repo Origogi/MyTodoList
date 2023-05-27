722b778557f350e0317df826df9653607fdeeb49
tony.origogi <tony.origogi@kakaobrain.com>
2023-05-27 09:18:21 +0900

    update

--- node_js/6-express/app.js
+++ node_js/6-express/app.js
@@ -2,15 +2,15 @@ import express from 'express';
 const app = express();
 
 
-app.get('/sky/:id',(req, res, next) => {
-    console.log(req.params);
-    console.log(req.params.id);
+app.get('/', (req, res, next) => {
+    console.log('first');
+    next();
+}, (req, res) => {
+    console.log('first2');
+});
 
-    console.log(req.query);
-    console.log(req.query.keyword);
-
-    res.setHeader('key', 'value');
-    res.status(201).send('Hello');
+app.get('/', (req, res) => {
+    console.log('second');
 });
 
 app.listen(8080);
