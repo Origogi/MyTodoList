36ca31296e3ae87b3e3b9ac50e39d9794e2940f9
tony.origogi <tony.origogi@kakaobrain.com>
2023-06-10 10:50:36 +0900

    Impl /tweets router , GET, POST

--- node_js/dweet/server/router/tweets.js
+++ node_js/dweet/server/router/tweets.js
@@ -1,20 +1,30 @@
 import express from "express";
 import 'express-async-errors';
 
-const tweets = [{
+let tweets = [{
     id: '1',
-    text: 'Hello Origogi!',
-    createdAt: Date.now().toString(),
+    text: 'Hello Bob!',
+    createdAt: new Date(),
     name: 'Bob',
     username: 'bob',
+    url: "https://pbs.twimg.com/profile_images/1377594486683955203/LWhdzk2f_400x400.jpg"
+}, {
+    id: '2',
+    text: 'Hello Origogi!',
+    createdAt: new Date(),
+    name: 'Origogi',
+    username: 'origogi',
     url: "https://pbs.twimg.com/profile_images/1377594486683955203/LWhdzk2f_400x400.jpg"
 }];
 
 const router = express.Router();
 
+// GET /tweets
+// GET /tweets?username=:username
 router.get('/', (req, res, next) => {
-    console.log(req.query);
     const userName = req.query.username;
+    console.log(userName);
+
     const data = userName ?
         tweets.filter(t => t.username === userName) :
         tweets;
@@ -22,10 +32,33 @@ router.get('/', (req, res, next) => {
     res.status(200).json(data);
 });
 
-// GET /tweets
-// GET /tweets?username=:username
 // GET /tweets/:id
+router.get('/:id', (req, res, next) => {
+    const param = req.params.id;
+    if (param) {
+        const data = tweets.find(t => t.id === param);
+        if (data) {
+            res.status(200).json(data);
+            return;
+        }
+    }
+    res.status(404).json({ message: `Tweet id(${param}) not found` });
+});
+
 // POST /tweets
+router.post('/', (req, res, next) => {
+    const { text, name, username } = req.body;
+    const tweet = {
+        id: Date.now().toString(),
+        createdAt: new Date(),
+        text,
+        name,
+        username,
+    }
+    tweets = [tweet, ...tweets];
+    res.status(201).json(tweet);
+});
+
 // PUT /tweets/:id
 // DELETE /tweets/:id
 
