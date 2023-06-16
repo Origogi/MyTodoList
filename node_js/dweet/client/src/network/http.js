60b1d3f8c3897db845d00122a24877db0f7755aa
tony.origogi <tony.origogi@kakaobrain.com>
2023-06-16 09:34:24 +0900

    Refactor client

---
+++ node_js/dweet/client/src/network/http.js
@@ -0,0 +1,31 @@
+export default class HttpClient {
+    constructor(baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    async fetch(url, options) {
+        console.log(url);
+
+        const res = await fetch(`${this.baseURL}${url}`, {
+            ...options,
+            headers: {
+                'Content-Type': "application/json",
+                ...options.headers,
+            },
+        });
+        console.log(res);
+
+        let data;
+        try {
+            data = await res.json();
+        } catch (e) {
+            console.error(e);
+        }
+
+        if (res.status > 299 || res.status < 200) {
+            const message = data && data.message ? data.message : "Something went wrong!";
+            throw new Error(message);
+        }
+        return data;
+    }
+}
