f7acf648b0360f0d5cfda6eb68bda25ac73d1dec
tony.origogi <tony.origogi@kakaobrain.com>
2023-07-04 10:44:48 +0900

    Add check auth

--- node_js/dweet/server/controller/tweet.js
+++ node_js/dweet/server/controller/tweet.js
@@ -33,19 +33,50 @@ export async function createTweet(req, res) {
 export async function updateTweet(req, res) {
   const id = req.params.id ?? "";
   const text = req.body.text ?? "";
-  const tweet = await tweetRepository.update(id, text);
+  const tweet = await tweetRepository.getById(id);
 
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
+  if (!tweet) {
     res.status(404).json({
       message: `Tweet id(${id}) not found`,
     });
+    return;
+  }
+
+  if (tweet.userId !== req.userId) {
+    res.status(403).json({
+      message: `Unauthorized to update the tweet`,
+    });
+    return;
+  }
+
+  const updated = await tweetRepository.update(id, text);
+
+  if (!updated) {
+    res.status(404).json({
+      message: `Tweet id(${id}) not found`,
+    });
+  } else {
+    res.status(200).json(updated);
   }
 }
 
 export async function deleteTweet(req, res) {
   const id = req.params.id ?? "";
+
+  const tweet = await tweetRepository.getById(id);
+
+  if (!tweet) {
+    return res.status(404).json({
+      message: `Tweet id(${id}) not found`,
+    });
+  }
+
+  if (tweet.userId !== req.userId) {
+    return res.status(403).json({
+      message: `Unauthorized to delete the tweet`,
+    });
+  }
+
   await tweetRepository.remove(id);
   res.sendStatus(204);
 }
