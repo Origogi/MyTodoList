7703d6ee0ce5a998dd392d1f2f8aaa58e5f8bdc5
tony.origogi <tony.origogi@kakaobrain.com>
2023-07-14 21:26:10 +0900

    Adjust mongoose in auth

--- node_js/dweet/server/db/database.js
+++ node_js/dweet/server/db/database.js
@@ -1,20 +1,34 @@
-import MongoDb from "mongodb";
+// import MongoDb from "mongodb";
+import Mongoose from 'mongoose';
+
 import { config } from "../config.js";
-import SQ from "sequelize";
+// import SQ from "sequelize";
 
-const { host, user, database, password } = config.db;
-export const sequelize = new SQ.Sequelize(database, user, password, {
-  host,
-  dialect: "mysql",
-});
+// const { host, user, database, password } = config.db;
+// export const sequelize = new SQ.Sequelize(database, user, password, {
+//   host,
+//   dialect: "mysql",
+// });
 
-let db;
 export async function connectDB() {
-  return MongoDb.MongoClient.connect(config.db.host).then((client) => {
-    db = client.db();
+  // return MongoDb.MongoClient.connect(config.db.host).then((client) => {
+  //   db = client.db();
+  // });
+  return Mongoose.connect(config.db.host).then(() => {
+    console.log("MongoDB connected!");
   });
 }
 
+export function useVirtualId(schema) {
+  // _id -> id
+  schema.virtual('id').get(function() {
+    return this._id.toString();
+  });
+  schema.set('toJSON', { virtuals: true });
+  schema.set('toObject', { virtuals: true });
+}
+
+let db;
 export function getUsers() {
   return db.collection("users");
 }
