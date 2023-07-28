e24a87aad8b84d1cb57e101b0390de33570fb463
tony.origogi <tony.origogi@kakaobrain.com>
2023-07-28 15:17:33 +0900

    Add Unit test async

--- TDD/3.8-unit-basic/src/test/async.test.js
+++ TDD/3.8-unit-basic/src/test/async.test.js
@@ -3,8 +3,27 @@ const fetchProduct = require("../async.js");
 describe("Async", () => {
   it("async test", (done) => {
     fetchProduct().then((product) => {
-      expect(product).toEqual({ item: "Milk", price: 100 });
+      expect(product).toEqual({ item: "Milk", price: 200 });
       done();
     });
+  });
+
+  it('async -return ', () => {
+    return fetchProduct().then((product) => {
+      expect(product).toEqual({ item: "Milk", price: 200 });
+    });
+  });
+
+  it('async - await', async () => {
+    const product = await fetchProduct();
+    expect(product).toEqual({ item: "Milk", price: 200 });
+  });
+
+  it('async - resolves', () => {
+    return expect(fetchProduct()).resolves.toEqual({ item: "Milk", price: 200 });
+  });
+
+  it('async - rejects', () => {
+    return expect(fetchProduct('error')).rejects.toEqual('network error');
   });
 });
