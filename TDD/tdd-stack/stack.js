afd65ae971b8b2c931c7bb1faa3ceeb38d75ad96
tony.origogi <tony.origogi@kakaobrain.com>
2023-08-10 09:53:05 +0900

    Update

--- TDD/tdd-stack/stack.js
+++ TDD/tdd-stack/stack.js
@@ -1,29 +1,37 @@
 class Stack {
 
   constructor() {
-    this.array = [];
+    this._size = 0;
+    this.head = null;
   }
 
   size() {
-    return this.array.length;
+    return this._size;
   }
 
   push(item) {
-    this.array.push(item);
+    this.head = {
+      item,
+      next: this.head,
+    };
+    this._size++;
   }
 
   pop() {
-    if (this.size() === 0) {
+    if (this._size === 0) {
       throw new Error('Stack is empty');
     }
-    return this.array.pop();
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.item;
   }
 
   peek() {
     if (this.size() === 0) {
       throw new Error('Stack is empty');
     }
-    return this.array[this.size() - 1];
+    return this.head.item;
   }
 }
 
