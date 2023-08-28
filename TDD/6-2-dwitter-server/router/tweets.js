15ef036c4e4e2076b1d70064e62c791d27a571d9
tony.origogi <tony.origogi@kakaobrain.com>
2023-08-28 18:36:24 +0900

    update

--- TDD/6-2-dwitter-server/router/tweets.js
+++ TDD/6-2-dwitter-server/router/tweets.js
@@ -13,23 +13,21 @@ const validateTweet = [
     .withMessage('text should be at least 3 characters'),
   validate,
 ];
-
 export default function tweetsRouter(tweetController) {
-// GET /tweet
-// GET /tweets?username=:username
+  // GET /tweet
+  // GET /tweets?username=:username
   router.get('/', isAuth, tweetController.getTweets);
 
-// GET /tweets/:id
+  // GET /tweets/:id
   router.get('/:id', isAuth, tweetController.getTweet);
 
-// POST /tweeets
+  // POST /tweeets
   router.post('/', isAuth, validateTweet, tweetController.createTweet);
 
-// PUT /tweets/:id
+  // PUT /tweets/:id
   router.put('/:id', isAuth, validateTweet, tweetController.updateTweet);
 
-// DELETE /tweets/:id
+  // DELETE /tweets/:id
   router.delete('/:id', isAuth, tweetController.deleteTweet);
+  return router;
 }
-
-export default router;
