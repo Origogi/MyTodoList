b8e6fd75e205a6ab19918f41dfdbd941f3f70478
tony.origogi <tony.origogi@kakaobrain.com>
2023-08-30 21:17:39 +0900

    update

--- TDD/6-2-dwitter-server/tests/integration-tests/auth.test.js
+++ TDD/6-2-dwitter-server/tests/integration-tests/auth.test.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import { startServer, stopServer } from '../../app.js';
-import { sequelize } from '../../db/database.js';
+import axios from "axios";
+import { startServer, stopServer } from "../../app.js";
+import { sequelize } from "../../db/database.js";
 import { faker } from "@faker-js/faker";
 
-describe('Auth APIs', () => {
+describe("Auth APIs", () => {
   let server;
   let request;
   beforeAll(async () => {
     server = await startServer();
     request = axios.create({
-      baseURL: 'http://localhost:8080',
+      baseURL: "http://localhost:8080",
       validateStatus: null,
     });
   });
@@ -19,19 +19,36 @@ describe('Auth APIs', () => {
     await stopServer(server);
   });
 
-  describe('POST to /auth/signup', () => {
-    it('returns 201 and auth token when user details are valid', async () => {
-      const user = {
-        name: faker.internet.displayName(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      };
+  describe("POST to /auth/signup", () => {
+    it("returns 201 and auth token when user details are valid", async () => {
+      const user = makeValidUser();
 
-      const res = await request.post('/auth/signup', user);
+      const res = await request.post("/auth/signup", user);
 
       expect(res.status).toBe(201);
       expect(res.data.token.length).toBeGreaterThan(0);
     });
+
+    it("returns 409 when username is already exists", async () => {
+      const user = makeValidUser();
+
+      let res = await request.post("/auth/signup", user);
+
+      expect(res.status).toBe(201);
+      expect(res.data.token.length).toBeGreaterThan(0);
+
+      res = await request.post("/auth/signup", user);
+      expect(res.status).toBe(409);
+      expect(res.data.message).toBe(`${user.username} already exists`);
+    });
   });
 });
+
+function makeValidUser() {
+  return {
+    name: faker.internet.displayName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
