61d5c540ac9f5ef3c2f3540a21210e626d5f7293
tony.origogi <tony.origogi@kakaobrain.com>
2023-09-01 21:29:12 +0900

    Update

--- TDD/6-2-dwitter-server/tests/integration-tests/auth.test.js
+++ TDD/6-2-dwitter-server/tests/integration-tests/auth.test.js
@@ -174,6 +174,141 @@ describe("Auth APIs", () => {
           text,
         });
       });
+
+      it("returns 400 when tweet text is less than 3 characters", async () => {
+        const text = faker.string.alphanumeric(2);
+        const user = await createNewUserAccount();
+
+        const res = await request.post(
+          "/tweets",
+          {
+            text,
+          },
+          {
+            headers: { Authorization: `Bearer ${user.jwt}` },
+          }
+        );
+
+        expect(res.status).toBe(400);
+        expect(res.data.message).toBe("text should be at least 3 characters");
+      });
+    });
+
+    describe("GET to /tweets", () => {
+      it("returns all tweets when username is not specified in the query", async () => {
+        const text = faker.string.alphanumeric(3);
+
+        const user1 = await createNewUserAccount();
+        const user2 = await createNewUserAccount();
+
+        const user1Headers = { Authorization: `Bearer ${user1.jwt}` };
+        const user2Headers = { Authorization: `Bearer ${user2.jwt}` };
+
+        await request.post("/tweets", { text }, { headers: user1Headers });
+        await request.post("/tweets", { text }, { headers: user2Headers });
+
+        const res = await request.get("/tweets", { headers: user1Headers });
+
+        expect(res.status).toBe(200);
+        expect(res.data.length).toBeGreaterThanOrEqual(2);
+      });
+
+      it("returns only tweets of the given user when username is specified in the query", async () => {
+        const text = faker.string.alphanumeric(3);
+
+        const user1 = await createNewUserAccount();
+        const user2 = await createNewUserAccount();
+
+        const user1Headers = { Authorization: `Bearer ${user1.jwt}` };
+        const user2Headers = { Authorization: `Bearer ${user2.jwt}` };
+
+        await request.post("/tweets", { text }, { headers: user1Headers });
+        await request.post("/tweets", { text }, { headers: user2Headers });
+
+        const res = await request.get("/tweets", {
+          headers: user1Headers,
+          param: {
+            username: user1.username,
+          },
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.data.length).toBeGreaterThanOrEqual(1);
+      });
+    });
+
+    describe("GET to /tweets/:id", () => {
+      it("returns 404 when tweet id does not exist", async () => {
+        const user = await createNewUserAccount();
+
+        const res = await request.get("/tweets/1234567890", {
+          headers: { Authorization: `Bearer ${user.jwt}` },
+        });
+
+        expect(res.status).toBe(404);
+      });
+
+      it("returns 200 and the tweet object when tweet id exists", async () => {
+        const text = faker.string.alphanumeric(3);
+
+        const user1 = await createNewUserAccount();
+        const user1Headers = { Authorization: `Bearer ${user1.jwt}` };
+
+        const createdTweet = await request.post(
+          "/tweets",
+          { text },
+          { headers: user1Headers }
+        );
+
+        const res = await request.get(`/tweets/${createdTweet.data.id}`, {
+          headers: user1Headers,
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.data.text).toBe(text);
+      });
+    });
+
+    describe("PUT to /tweets/:id", () => {
+      it("returns 404 when tweet id does not exist", async () => {
+        const user = await createNewUserAccount();
+        const text = faker.string.alphanumeric(3);
+
+        const res = await request.put(
+          "/tweets/1234567890",
+          { text: text },
+          { headers: { Authorization: `Bearer ${user.jwt}` } }
+        );
+
+        expect(res.status).toBe(404);
+      });
+
+      it("returns 200 and updated tweet when tweet id exists and the tweet belongs to the user", async () => {
+        const text = faker.string.alphanumeric(3);
+        const updatedText = faker.string.alphanumeric(3);
+        const user = await createNewUserAccount();
+
+        const createdTweet = await request.post(
+          "/tweets",
+          {
+            text,
+          },
+          {
+            headers: { Authorization: `Bearer ${user.jwt}` },
+          }
+        );
+
+        const res = await request.put(
+          `/tweets/${createdTweet.data.id}`,
+          { text: updatedText },
+          {
+            headers: { Authorization: `Bearer ${user.jwt}` },
+          }
+        );
+
+        expect(res.status).toBe(200);
+        expect(res.data.text).toBe(updatedText);
+      });
     });
   });
 });
